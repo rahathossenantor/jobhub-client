26791bd6796997bdc8112fd9a5d657f9fe8d21e5
mdrahathossenantor <md.rahathossenantor@gmail.com>
2023-11-05 23:41:59 +0600

    Footer section created

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "./components/NavBar";
+import FooterComponent from "./components/FooterComponent";
 
 function App() {
   return (
     <div>
       <NavBar></NavBar>
       <Outlet></Outlet>
+      <FooterComponent></FooterComponent>
     </div>
-  )
+  );
 }
 
 export default App;
